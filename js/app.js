277e15099f03e1e97d748ff47b4c8fbb1a855788
Streum <hotton.jerem@gmail.com>
2024-05-03 09:38:58 +0200

    CSS ok jouer modif titre h2 + js compteur de coup + reset de jeu

--- js/app.js
+++ js/app.js
@@ -4,15 +4,67 @@
 // au groupe de sélecteurs spécifiés.
 const cartes = document.querySelectorAll('.case');
 
+const titreNombreDeCoups = document.getElementById('coups');
+
 let hasCarteFlip = false;
 let premiereCarte;
 let deuxiemeCarte;
 // le verrou sert a pas retourner plus de 2 carte
 let verrou = false;
-let nombreDeCoup;
+let nombreDeCoup = 0;
+
+// le nombre de carte divisé par 2 pour le nombre de pair
+let nombreDePairRestantes = cartes.length / 2;
+
+// début de notre jeu
+startGame();
+
+function startGame() {
+    // on ajoute un event de click :) 
+    cartes.forEach(carte => carte.addEventListener('click', retournerCarte));
+    shuffleCards();
+    afficheScore();
+}
+
+function afficheScore() {
+    titreNombreDeCoups.innerHTML = `Nombre de coups : ${nombreDeCoup}`
+}
+
+function afficheRestart() {
+    titreNombreDeCoups.innerHTML = `GAME EN COURS DE RESET...`
+}
+
+function restartGame() {
+    // on remet le nombre de pair par défaut
+    nombreDePairRestantes = cartes.length / 2;
+
+    afficheRestart();
+
+    cartes.forEach(carte => carte.classList.remove('flip'));
+
+    resetPlateau();
+    
+    nombreDeCoup = 0;
+
+    // attendre 1.5 seconde pour l'animation avant de relancer la partie
+    setTimeout(() => {
+        startGame();
+    }, 1500);
+    
+}
+
+function shuffleCards() {
+    cartes.forEach(carte => {
+        let ramdomPos = Math.floor(Math.random() * 12);
+        carte.style.order = ramdomPos;
+    });
+}
 
 function retournerCarte() {
+    
     if (verrou) return;
+    // ici je retourne une carte
+    // this = une carte du plateau (une div)
     if (this === premiereCarte) return;
 
     this.classList.add('flip');
@@ -21,28 +73,39 @@ function retournerCarte() {
         hasCarteFlip = true;
         premiereCarte = this;
         nombreDeCoup ++;
+        afficheScore();
+    
         return;
     }
 
     deuxiemeCarte = this;
     verrou = true;
 
-    correspondance();
+    verifieLesCorrespondances();
 }
 
-function correspondance() {
+function verifieLesCorrespondances() {
     if (premiereCarte.dataset.legume === deuxiemeCarte.dataset.legume) {
-        annuleCarte();
+        desactiveLesCartes();
         return;
     }
 
     reflipCarte();
 }
 
-function annuleCarte() {
+function desactiveLesCartes() {
+
     premiereCarte.removeEventListener('click', retournerCarte);
     deuxiemeCarte.removeEventListener('click', retournerCarte);
-    resetPlateau();
+
+    nombreDePairRestantes--;
+
+    if (nombreDePairRestantes !== 0) {
+        resetPlateau();
+        return;
+    }
+
+    titreNombreDeCoups.innerHTML = `Bien joué beau gosse ! Tu as fini en seulement ${nombreDeCoup} coups !`;
 }
 
 function reflipCarte() {
@@ -59,12 +122,18 @@ function resetPlateau() {
     [premiereCarte, deuxiemeCarte] = [null, null];
 }
 
-(function shuffle() {
+// Fonction pour relancer une partie de memory
+function relancerPartieMemory() {
+    console.log("La touche espace a été pressée. La partie de memory est relancée !");
+    // Mettre ici le code pour relancer la partie de memory
 
-    cartes.forEach(carte => {
-        let ramdomPos = Math.floor(Math.random() * 12);
-        carte.style.order = ramdomPos;
-    });
-})();
+    startGame();
+}
 
-cartes.forEach(carte => carte.addEventListener('click', retournerCarte));
+// Écouter l'événement de pression de la touche espace
+document.addEventListener("keydown", function(event) {
+    console.log("Touche pressée : ", event.key);
+    if (event.key === " ") {
+        restartGame();
+    }
+});
